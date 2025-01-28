6ecbb3c38e7079d83bb3e2858f5e468e337cf2eb
Ronhem <rayandijezas12s@gmail.com>
2025-01-28 03:46:13 -0500

    Parallax Infinito
    Corrigi o bug do parallax e adicionei um parallax infinito.

--- Assets/Scripts/Game/ParallaxScroller.cs
+++ Assets/Scripts/Game/ParallaxScroller.cs
@@ -1,52 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ParallaxScroller : MonoBehaviour
 {
     public float speed = 0.3f; // Velocidade do movimento no eixo Y
-    private float spriteHeight; // Altura do sprite para reposição
-    private List<Transform> sprites; // Lista de todos os filhos (sprites)
+    public Transform parallaxController; //Objeto que fará o efeito parallax
+    public Transform parallaxInfinito;//Objeto que será loopado no parallax
+    [SerializeField]private float parallaxHeight; //Variavel que define a altura do parallax para looping
 
+    // Start is called before the first frame update
     void Start()
     {
-        // Calcula a altura do primeiro sprite
-        spriteHeight = GetComponentInChildren<SpriteRenderer>().bounds.size.y;
 
-        // Armazena todos os filhos (sprites) na lista
-        sprites = new List<Transform>();
-        foreach (Transform child in transform)
-        {
-            sprites.Add(child);
-        }
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Move todos os sprites para baixo no eixo Y
-        foreach (Transform child in sprites)
-        {
-            child.position -= new Vector3(0, speed * Time.deltaTime, 0);
-        }
-
-        // Verifica se o último sprite saiu da tela e reposiciona os sprites
-        if (sprites[0].position.y < -spriteHeight)
-        {
-            RepositionSprites();
-        }
+        parallaxController.position -= new Vector3(0, speed * Time.deltaTime, 0);
     }
-
-    // Reposiciona os sprites para criar o efeito contínuo
-    void RepositionSprites()
+    private void FixedUpdate()
     {
-        // Move o primeiro sprite para a posição do último
-        Transform firstSprite = sprites[0];
-        Transform lastSprite = sprites[sprites.Count - 1];
-
-        firstSprite.position = lastSprite.position + new Vector3(0, spriteHeight, 0);
-
-        // Remove o primeiro sprite da lista e coloca ele no final
-        sprites.RemoveAt(0);
-        sprites.Add(firstSprite);
+        parallaxHeight = parallaxInfinito.transform.position.y;
+        if (parallaxHeight < -12f)
+        {
+            parallaxInfinito.transform.position = new Vector2(0, 12f);
+        }
     }
 }
